ac81cf364641dddfb384fa81b7af7b69fa0785d0
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2022-08-08 18:22:13 -0300

    Ultimo commit

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ conn.sync({ force: false }).then(async () => {
       dbLoader();
     }
 
-  server.listen(process.env.PORT || 5000, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 
